d9a7fb4ac27f0b09b8e23f01841b0d03c3dcfdbf
Affan Ahammed <116718291+affangitty@users.noreply.github.com>
2024-03-24 03:03:48 +0530

    js update

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-var a=Math.floor(Math.random()*6)+1;
-var b=Math.floor(Math.random()*6)+1;
-document.querySelector(".btn").addEventListener("click",
-   function (){
-document.querySelector(".img1").setAttribute("src",`./images/dice${a}.png`);
-document.querySelector(".img2").setAttribute("src",`./images/dice${b}.png`);
-if(a>b){
-    document.querySelector("h1").innerHTML="Player 1 Wins!";
+abc();
+function abc() {
+    var a = Math.floor(Math.random() * 6) + 1;
+    var b = Math.floor(Math.random() * 6) + 1;
+  document.querySelector(".btn").addEventListener("click", function () {
+    document
+      .querySelector(".img1")
+      .setAttribute("src", `./images/dice${a}.png`);
+    document
+      .querySelector(".img2")
+      .setAttribute("src", `./images/dice${b}.png`);
+    if (a > b) {
+      document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    } else if (a < b) {
+      document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    } else {
+      document.querySelector("h1").innerHTML = "Its a Tie!";
+    }
+    abc();
+});
 }
-else if(a<b){
-    document.querySelector("h1").innerHTML="Player 2 Wins!";
-}
-else{
-    document.querySelector("h1").innerHTML="Its a Tie!";
-}})
